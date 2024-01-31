3da2cafa299bf8be31e3e7f839d3c6e1b397d995
Sergeeva Aleksandra <sergeevaaleksandra743@gmail.com>
2024-01-31 16:09:24 +0300

    changed to utf8

--- Creating a menu/main.cpp
+++ Creating a menu/main.cpp
@@ -1,4 +1,4 @@
-#include<clocale>
+п»ї#include<clocale>
 #include<iostream>
 
 #include"menu.hpp"
@@ -8,21 +8,21 @@
 int main() {
 	std::setlocale(LC_ALL, "");
 
-	AS::MenuItem read = {"1 - Русские писатели", AS::read};
-	AS::MenuItem exit = { "0 - Завершить чтение", AS::exit};
+	AS::MenuItem read = {"1 - Р СѓСЃСЃРєРёРµ РїРёСЃР°С‚РµР»Рё", AS::read};
+	AS::MenuItem exit = { "0 - Р—Р°РІРµСЂС€РёС‚СЊ С‡С‚РµРЅРёРµ", AS::exit};
 
 	AS::MenuItem* main_children[] = { &exit, &read };
 	const int main_size = sizeof(main_children) / sizeof(main_children[0]);
 
 	int user_input;
 	do {
-		std::cout << "Выберите книгу для чтения" << std::endl;
+		std::cout << "Р’С‹Р±РµСЂРёС‚Рµ РєРЅРёРіСѓ РґР»СЏ С‡С‚РµРЅРёСЏ" << std::endl;
 		for (int i = 1; i < main_size; i++) {
 			std::cout << main_children[i]->title << std::endl;
 
 		}
 		std::cout << main_children[0]->title << std::endl;
-		std::cout << "Выбор книги >";
+		std::cout << "Р’С‹Р±РѕСЂ РєРЅРёРіРё >";
 
 		std::cin >> user_input;
 		main_children[user_input]->func();
